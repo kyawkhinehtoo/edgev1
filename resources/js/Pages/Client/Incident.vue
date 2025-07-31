5280b58acb8da615136b0b0054499dd495fc93b5
Kyaw Khine Htoo <alberthtoo.k@gmail.com>
2025-07-31 09:42:27 +0630

    go back

--- resources/js/Pages/Client/Incident.vue
+++ resources/js/Pages/Client/Incident.vue
@@ -1275,6 +1275,7 @@ export default {
               title: page.props.flash.message,
             });
             loading.value = false;
+              closeModal();
           },
           onError: (errors) => {
             loading.value = false;
